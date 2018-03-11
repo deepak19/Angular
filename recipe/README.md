cb8895f08dc433a7b0ec2e21a3e8528b1ebeeb2d
Deepak Kumar <deepakkumar@Deepak19Mac.local>
2018-03-12 02:53:12 +0530

    Recipe New Setup

--- recipe/README.md
+++ recipe/README.md
@@ -1,4 +1,4 @@
-# Recepie
+# Recipe
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.1.
 
--- recipe/e2e/app.e2e-spec.ts
+++ recipe/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('recepie App', () => {
+describe('recipe App', () => {
   let page: AppPage;
 
   beforeEach(() => {
